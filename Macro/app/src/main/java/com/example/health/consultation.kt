1d1edbef20cd44279e866a97b86f5f7e1bbdc3cc
Aldio Yohanes <aldioguire@gmail.com>
2023-10-17 11:07:33 +0700

    Project Macro

---
+++ Macro/app/src/main/java/com/example/health/consultation.kt
@@ -0,0 +1,11 @@
+package com.example.health
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class consultation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consultation)
+    }
+}
